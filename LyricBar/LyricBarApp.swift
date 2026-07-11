cd691cd55d5d6026b388c372117c8e14dc4b4155
aidenlim17 <peachesaiden@gmail.com>
2026-07-12 00:44:27 +0900

    Initial Commit

---
+++ LyricBar/LyricBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  LyricBarApp.swift
+//  LyricBar
+//
+//  Created by aiden on 7/12/26.
+//
+
+import SwiftUI
+
+@main
+struct LyricBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
